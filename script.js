f9ac3a7fc8f1ac3f092cf4bb5e71a258afa5e0b1
corrinchere <96142555+corrinchere@users.noreply.github.com>
2021-12-28 10:18:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+console.log('hellooo')
+
+let button = document.querySelector('#submitButton')
+
+// let button= document.querySelector('#heightButton')
+
+
+async function getData (event) {
+    event.preventDefault
+    let textInput = document.querySelector('#inputBar').value
+    const url = `https://pokeapi.co/api/v2/pokemon/${textInput}`
+
+
+
+fetch(url)
+
+    .then(res => {
+        return res.json();
+    })
+    .then(res => {
+        const pokemonHeader = document.querySelector('#pokemonName')
+        pokemonHeader.innerText = res.name.toUpperCase()
+
+        const pokePic = document.querySelector('#pokemonImg')
+        pokePic.src = res.sprites.front_default
+
+        const pokemonHeight = document.querySelector('#pokemonHeight')
+        pokemonHeight.innerText= ('Height: ') + res.height
+
+        const pokemonWeight = document.querySelector('#pokemonWeight')
+        pokemonWeight.innerText = ('Weight: ') + res.weight
+
+        // const pokemonWeight = document.querySelector('#pokemonWeight')
+        // pokemonWeight.innerText = ('Weight: ') + res.weight
+
+        
+// to grab image, need to model after html src and then follow format
+
+        
+
+
+
+        console.log("success!", res)
+
+    } )
+    .catch(err => {
+        console.log('error', err)
+
+    })
+
+}
+
+button.addEventListener('click', getData)
+
+    // two ways to write functions, bottom one is more modern and elegantly supposedly...
+// function sayHello () {}
+
+// const sayGoodbye = () => {}
